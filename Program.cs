92b28838c88f8a1636830d717551404a38c7679d
danieljmartins <danieljjmartins@gmail.com>
2025-01-24 19:52:42 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace didaticos.redimensionador
+{
+    internal class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Iniciando Redimencionador");
+
+            Thread thread = new Thread(Redimensionar);
+            thread.Start();
+
+            Console.WriteLine("Tecle para fechar");
+            Console.Read();
+        }
+
+        static void Redimensionar()
+        {
+            string diretorio_entrada = "Arquivos_Entrada";
+
+            if (!Directory.Exists(diretorio_entrada))
+            {
+                Directory.CreateDirectory(diretorio_entrada);
+            }
+
+            while (true)
+            {
+                Thread.Sleep ( new TimeSpan ( 0, 0, 3 ) );
+            }
+        }
+    }
+}
